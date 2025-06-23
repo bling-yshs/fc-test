b693799cbf4dab032aeb80f2f75364d8bb80a199
bling-yshs <377178599@qq.com>
2025-06-23 15:41:55 +0800

    init

--- start.sh
+++ start.sh
@@ -2,8 +2,5 @@
 # 安装依赖到本地
 pip install -r requirements.txt
 
-# 设置库路径
-export PYTHONPATH=/code/.python_packages:$PYTHONPATH
-
 # 启动 HTTP 服务，监听 0.0.0.0:9000（FC 默认 CAPort）
 exec uvicorn api:app --host 0.0.0.0 --port 9000
